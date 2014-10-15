e8bf217b3c5f584cd415909cb5abfb303faeb648
kyubuns <jp@ex.cx>
2014-10-16 00:30:29 +0900

    Basic Exampleシーン追加

---
+++ Examples/Scripts/BasicViewModel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DataBinderForNGUI.Example
+{
+
+public class BasicViewModel : MonoBehaviour
+{
+  [SerializeField] List<ReferenceModel> views;
+  RPGStatusModel model;
+
+  void Awake()
+  {
+    model = new RPGStatusModel("kyubuns", 10, 20, 5);
+    BinderTools.ApplyModel(views, model);
+  }
+}
+
+}
